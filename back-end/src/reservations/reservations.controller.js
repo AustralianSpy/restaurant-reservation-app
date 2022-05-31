e86c6c7edbed72db3df816ee31f3336dfbeab2a6
Sydney Workman <sworkman1996@gmail.com>
2022-05-30 22:43:26 -0500

    create new reserv routing / control

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,5 +1,15 @@
 const service = require("./reservations.service");
-const asyncErrorBoundary =  require("../errors/asyncErrorBoundary");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const hasProperties = require("../errors/hasProperties");
+
+const properties = [
+    "first_name",
+    "last_name",
+    "mobile_number",
+    "reservation_date",
+    "reservation_time",
+    "people",
+]
 
 async function list(req, res) {
     // Request list of all reservations.
@@ -8,6 +18,28 @@ async function list(req, res) {
     res.json({ data: reservations });
 }
 
+async function create(req, res) {
+    const { data = {} } = req.body;
+    const newReservation = service.create(data);
+
+    /* const { data: { reservation_id, first_name, last_name, mobile_number, reservation_date, reservation_time, people } = {} } = req.body;
+
+    const newReservation = {
+        reservation_id,
+        first_name,
+        last_name,
+        mobile_number,
+        reservation_date,
+        reservation_time,
+        people
+    };
+
+    const response = await service.create(newReservation); */
+
+    res.status(201).json({ data: newReservation });
+}
+
 module.exports = {
     list: asyncErrorBoundary(list),
+    create: [hasProperties(properties), asyncErrorBoundary(create)],
 };
--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -2,17 +2,25 @@ const knex = require('../db/connection');
 
 // Queries for list of all reservations.
 function list() {
-    return knex('reservations').select('*');
+    return knex("reservations").select("*");
 }
 
 // Queries for list of all reservations on a certain date.
 function listByDate(date) {
-    return knex('reservations')
-        .select('*')
+    return knex("reservations")
+        .select("*")
         .where({ reservation_date: date });
+}
+
+function create(reservation) {
+    return knex("reservations")
+        .insert(reservation)
+        .returning("*")
+        .then((createdRecords) => createdRecords[0]);
 }
 
 module.exports = {
     list,
     listByDate,
+    create,
 };
