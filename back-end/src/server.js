fe6493f7d8e086a01b037abfda6db9ddcc4b97a3
Sydney Workman <sworkman1996@gmail.com>
2022-06-20 10:16:35 -0500

    deleted comments

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -3,19 +3,6 @@ const { PORT = 5001 } = process.env;
 const app = require("./app");
 const knex = require("./db/connection");
 
-/*
-knex.migrate
-  .latest()
-  .then((migrations) => {
-    console.log("migrations", migrations);
-    app.listen(PORT, listener);
-  })
-  .catch((error) => {
-    console.error(error);
-    knex.destroy();
-  });
-*/
-
 app.listen(PORT, listener);
 
 function listener() {
